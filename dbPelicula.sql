feed92226e4902e103592e3654971c815c59170a
Alexander9306 <alexander9306@gmail.com>
2019-12-22 01:05:03 -0400

    .

--- dbPelicula.sql
+++ dbPelicula.sql
@@ -1,10 +1,8 @@
 create database dbBiblioteca
 go
 
-
 use dbBiblioteca
 go
-
 --La imagen se guardara directamente en la BD para evitar extra configuraciones al gestor de Base de datos. 
 --Una solucion mas recomendable es utilzar FileStream para guardar las rutas de las imagenes
 
@@ -16,29 +14,23 @@ create table actor(
 );
 go
 create table actor_foto(
-	idfoto int  constraint pk_actor_foto primary key not null,
-	constraint fk_actor_foto references actor(idactor) ON DELETE CASCADE,
+	idfoto int constraint pk_actor_foto primary key not null,
+	constraint fk_actor_foto FOREIGN KEY (idfoto) references actor(idactor) ON DELETE CASCADE,
 	foto varbinary(max) null --Omitida la restriccion de tipo nulo para que se pueda crear la pelicula sin una foto en el momento
 ); 
-go
-
-
-create table pelicula_foto(
-	idfoto int  identity(1,1) constraint pk_pelicula_foto primary key not null,
-	foto varbinary(max) null --Omitida la restriccion de tipo nulo para que se pueda crear la pelicula sin una foto en el momento
-);
-						     
 go
 create table pelicula(
 	idpelicula int identity(1,1) constraint pk_pelicula primary key not null,
 	titulo varchar(100) not null constraint uc_pelicula_titulo unique,
 	genero varchar(60) not null,
-	fechaestreno date not null,
-	idfoto int not null, 
-	constraint fk_pelicula_foto foreign key (idfoto) 
-	references pelicula_foto(idfoto) ON DELETE CASCADE
+	fechaestreno date not null
 );
-
+go
+create table pelicula_foto(
+	idfoto int constraint pk_pelicula_foto primary key not null,
+	constraint fk_pelicula_foto FOREIGN KEY (idfoto) references pelicula(idpelicula) ON DELETE CASCADE,
+	foto varbinary(max) null --Omitida la restriccion de tipo nulo para que se pueda crear la pelicula sin una foto en el momento
+); 
 go
 create table elenco(
 	idelenco int  identity(1,1) constraint pk_elenco primary key not null,
